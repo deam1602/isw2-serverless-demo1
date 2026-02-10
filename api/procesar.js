6c0b952e92666bb3f42a1660c5fb1105582e9208
DiegoAzurdia <93228596+deam1602@users.noreply.github.com>
2026-02-09 18:22:44 -0600

    Update procesar.js

--- api/procesar.js
+++ api/procesar.js
@@ -1,15 +1,7 @@
 export default function handler(req, res) {
   const nombre = req.query.nombre || "anónimo";
 
-  if (nombre.toLowerCase() === "error") {
-    return res.status(500).json({
-      error: "Error recibido",
-      detalle: "Se recibió la palabra de fallo"
-    });
-  }
-  
   res.status(200).json({
-    resultado: `Nombre procesado: ${nombre.toUpperCase()}`,
-    timestamp: new Date().toISOString() 
+    resultado: `Nombre procesado: ${nombre.toUpperCase()}`
   });
 }
